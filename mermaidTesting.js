0cc14f62317d9df922085a06039fac573b99746a
austinTracy7 <101482688+austinTracy7@users.noreply.github.com>
2023-10-30 17:21:59 -0600

    Update mermaidTesting.js

--- mermaidTesting.js
+++ mermaidTesting.js
@@ -1,3 +1,11 @@
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
+
 %%{init: {"themeVariables": {"pie1": "#ff0000", "pie2": "#00ff00"} }}%%
 pie
     title Mega vs Non-Mega Pokemon
