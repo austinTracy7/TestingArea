0a5c72d6e8f54ee59857a52c8c0b6edecef2558c
austinTracy7 <101482688+austinTracy7@users.noreply.github.com>
2023-10-30 17:21:36 -0600

    Update mermaidTesting.js

--- mermaidTesting.js
+++ mermaidTesting.js
@@ -3,3 +3,20 @@ pie
     title Mega vs Non-Mega Pokemon
     "Mega" : 7
     "Non-Mega" : 9
+
+
+```mermaid
+%%{init: {"themeVariables": {"pie1": "#ff0000", "pie2": "#00ff00"} }}%%
+pie
+    title Mega vs Non-Mega Pokemon
+    "Mega" : 7
+    "Non-Mega" : 9
+```
+
+
+```mermaid
+pie
+    title Mega vs Non-Mega Pokemon
+    "Mega" : 7
+    "Non-Mega" : 9
+```
